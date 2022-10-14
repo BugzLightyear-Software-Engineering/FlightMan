b96b2282ab368acae6e539ac5f41647fe7e78ab8
Abhilash Venkatesh <abhilash.venkatesh@columbia.edu>
2022-10-14 16:36:41 -0400

    Set root mapping to Hello World

--- src/main/java/com/flightman/flightman/FlightmanApplication.java
+++ src/main/java/com/flightman/flightman/FlightmanApplication.java
@@ -14,7 +14,7 @@ public class FlightmanApplication {
 		SpringApplication.run(FlightmanApplication.class, args);
 	}
 
-        @GetMapping("/hello")
+        @GetMapping("/")
         public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
                 return String.format("Hello %s, the great!", name);
         }
