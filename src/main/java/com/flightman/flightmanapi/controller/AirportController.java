d3562711dde48c27b092526c0b268040242e6556
Miloni Atal <miloniatal@gmail.com>
2022-10-19 19:38:21 -0400

    Added generic /api context for application

--- src/main/java/com/flightman/flightmanapi/controller/AirportController.java
+++ src/main/java/com/flightman/flightmanapi/controller/AirportController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,6 @@ import com.flightman.flightmanapi.repositories.AirportRepository;
 // import com.flightman.flightmanapi.services.AirportService;
 
 @RestController
-@RequestMapping("/api")
 public class AirportController {
 
         @Autowired
